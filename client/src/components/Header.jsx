a1b79efdc7272f9793317443c9ac84e9932e6c04
pteminence-bit <pteminence@gmail.com>
2026-05-11 16:54:58 -0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,8 +29,8 @@ const Header = () => {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                <Link to='/' >
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>Staybnb</span>
-                    <span className='text-slate-700'>Estate</span>
+                    <span className='text-slate-500'>Dwell</span>
+                    <span className='text-slate-700'>Edge</span>
                 </h1>
                 </Link>
                 <form onSubmit={handleSubmit} className='bg-slate-100 p-3
